d6949fb5698abf303da205ca03c480a6995e4a11
Jitin <jitin.s.nair@gmail.com>
2019-07-07 20:07:54 +0530

    create output directory if it does not exist

--- 1d_diff_explicit/Diff1DExplicit.c
+++ 1d_diff_explicit/Diff1DExplicit.c
@@ -1,5 +1,56 @@
-#include "stdio.h"
-#include "math.h"
+#include <stdio.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+void write_to_VTK(nx, ny, nz, ){
+  // write data for every istep in VTK file format
+  int Nx=nx+1,
+  Ny=ny+1,
+  Nz=nz+1,
+  num_points=0;
+
+  num_points=Nx*Ny*Nz;
+
+  // create output directory if not created
+  struct stat st = {0};
+  if (stat("./output", &st) == -1) {
+    mkdir("./output", 0700);
+  }
+
+  char filename[30];
+  sprintf(filename, "./output/time_%05d.vtk", k);
+  fp = fopen(filename,"w");
+
+  // write header of VTK file
+  fprintf(fp, "# vtk DataFile Version 2.0\n");
+  fprintf(fp, "time_10.vtk\n");
+  fprintf(fp, "ASCII\n");
+
+  // co-ordinates of grid points
+  fprintf(fp, "DATASET STRUCTURED_GRID\n");
+  fprintf(fp, "DIMENSIONS %6d %6d %6d\n", Nx, Ny, Nz );
+  fprintf(fp, "POINTS %6d  float\n", num_points );
+  for (l=0; l<=N; l++){
+    float x, y, z;
+    x = l*dx;
+    y = l*dy;
+    z = l*dz;
+    fprintf(fp, "%f %f %f\n", x, y, z);
+  }
+
+  // grid point values
+  fprintf(fp,"POINT_DATA %6d\n", num_points);
+  fprintf(fp,"SCALARS CONC  float  1\n");
+  fprintf(fp,"LOOKUP_TABLE default\n");
+  for (l=0; l <= N; l++) {
+    fprintf(fp, "%f\n", conc[l]);
+  }
+
+  fclose(fp);
+}
 
 int main() {
 
@@ -18,25 +69,25 @@ int main() {
   istep=1000;
   nstep=200000;
   alpha=D*dt/(dx*dx);
+
   // file write parameters
   k=0;
   nx=N;
   ny=0;
   nz=0;
 
-
   //Initial conditions and profile
   float conc[N];
   conc[0]=1.0;
-    for (i=1; i<N; i++){
-        conc[i]=0.0;
-    }
+  for (i=1; i<N; i++){
+    conc[i]=0.0;
+  }
 
   // write initial concentration to file
   int Nx=nx+1,
-      Ny=ny+1,
-      Nz=nz+1,
-      num_points=0;
+  Ny=ny+1,
+  Nz=nz+1,
+  num_points=0;
 
   num_points=Nx*Ny*Nz;
 
@@ -88,44 +139,44 @@ int main() {
       conc[N]=(1-2*alpha)*conc[N]+2*alpha*(conc[N-1]);
     }
 
-      //Write results to file in fpointer after istep iterations
-      int Nx=nx+1,
-          Ny=ny+1,
-          Nz=nz+1,
-          num_points=0;
+    //Write results to file in fpointer after istep iterations
+    int Nx=nx+1,
+    Ny=ny+1,
+    Nz=nz+1,
+    num_points=0;
 
-      num_points=Nx*Ny*Nz;
+    num_points=Nx*Ny*Nz;
 
-      char filename[30];
-      sprintf(filename, "./output/time_%05d.vtk", k);
-      fp = fopen(filename,"w");
+    char filename[30];
+    sprintf(filename, "./output/time_%05d.vtk", k);
+    fp = fopen(filename,"w");
 
-      // write header of VTK file
-      fprintf(fp, "# vtk DataFile Version 2.0\n");
-      fprintf(fp, "time_10.vtk\n");
-      fprintf(fp, "ASCII\n");
+    // write header of VTK file
+    fprintf(fp, "# vtk DataFile Version 2.0\n");
+    fprintf(fp, "time_10.vtk\n");
+    fprintf(fp, "ASCII\n");
 
-      // co-ordinates of grid points
-      fprintf(fp, "DATASET STRUCTURED_GRID\n");
-      fprintf(fp, "DIMENSIONS %6d %6d %6d\n", Nx, Ny, Nz );
-      fprintf(fp, "POINTS %6d  float\n", num_points );
-      for (l=0; l<=N; l++){
-        float x, y, z;
-        x = l*dx;
-        y = l*dy;
-        z = l*dz;
-        fprintf(fp, "%f %f %f\n", x, y, z);
-      }
+    // co-ordinates of grid points
+    fprintf(fp, "DATASET STRUCTURED_GRID\n");
+    fprintf(fp, "DIMENSIONS %6d %6d %6d\n", Nx, Ny, Nz );
+    fprintf(fp, "POINTS %6d  float\n", num_points );
+    for (l=0; l<=N; l++){
+      float x, y, z;
+      x = l*dx;
+      y = l*dy;
+      z = l*dz;
+      fprintf(fp, "%f %f %f\n", x, y, z);
+    }
 
-      // grid point values
-      fprintf(fp,"POINT_DATA %6d\n", num_points);
-      fprintf(fp,"SCALARS CONC  float  1\n");
-      fprintf(fp,"LOOKUP_TABLE default\n");
-      for (l=0; l <= N; l++) {
-        fprintf(fp, "%f\n", conc[l]);
-      }
+    // grid point values
+    fprintf(fp,"POINT_DATA %6d\n", num_points);
+    fprintf(fp,"SCALARS CONC  float  1\n");
+    fprintf(fp,"LOOKUP_TABLE default\n");
+    for (l=0; l <= N; l++) {
+      fprintf(fp, "%f\n", conc[l]);
+    }
 
-      fclose(fp);
+    fclose(fp);
 
   }
 
