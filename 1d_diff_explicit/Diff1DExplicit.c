d2c052d8a41690818f49db935b1b6a9a667e1771
Jitin <jitin.s.nair@gmail.com>
2019-07-07 20:08:55 +0530

    commented function

--- 1d_diff_explicit/Diff1DExplicit.c
+++ 1d_diff_explicit/Diff1DExplicit.c
@@ -5,52 +5,52 @@
 #include <unistd.h>
 
 
-void write_to_VTK(nx, ny, nz, ){
-  // write data for every istep in VTK file format
-  int Nx=nx+1,
-  Ny=ny+1,
-  Nz=nz+1,
-  num_points=0;
-
-  num_points=Nx*Ny*Nz;
-
-  // create output directory if not created
-  struct stat st = {0};
-  if (stat("./output", &st) == -1) {
-    mkdir("./output", 0700);
-  }
-
-  char filename[30];
-  sprintf(filename, "./output/time_%05d.vtk", k);
-  fp = fopen(filename,"w");
-
-  // write header of VTK file
-  fprintf(fp, "# vtk DataFile Version 2.0\n");
-  fprintf(fp, "time_10.vtk\n");
-  fprintf(fp, "ASCII\n");
-
-  // co-ordinates of grid points
-  fprintf(fp, "DATASET STRUCTURED_GRID\n");
-  fprintf(fp, "DIMENSIONS %6d %6d %6d\n", Nx, Ny, Nz );
-  fprintf(fp, "POINTS %6d  float\n", num_points );
-  for (l=0; l<=N; l++){
-    float x, y, z;
-    x = l*dx;
-    y = l*dy;
-    z = l*dz;
-    fprintf(fp, "%f %f %f\n", x, y, z);
-  }
-
-  // grid point values
-  fprintf(fp,"POINT_DATA %6d\n", num_points);
-  fprintf(fp,"SCALARS CONC  float  1\n");
-  fprintf(fp,"LOOKUP_TABLE default\n");
-  for (l=0; l <= N; l++) {
-    fprintf(fp, "%f\n", conc[l]);
-  }
-
-  fclose(fp);
-}
+// void write_to_VTK(nx, ny, nz, ){
+//   // write data for every istep in VTK file format
+//   int Nx=nx+1,
+//   Ny=ny+1,
+//   Nz=nz+1,
+//   num_points=0;
+//
+//   num_points=Nx*Ny*Nz;
+//
+//   // create output directory if not created
+//   struct stat st = {0};
+//   if (stat("./output", &st) == -1) {
+//     mkdir("./output", 0700);
+//   }
+//
+//   char filename[30];
+//   sprintf(filename, "./output/time_%05d.vtk", k);
+//   fp = fopen(filename,"w");
+//
+//   // write header of VTK file
+//   fprintf(fp, "# vtk DataFile Version 2.0\n");
+//   fprintf(fp, "time_10.vtk\n");
+//   fprintf(fp, "ASCII\n");
+//
+//   // co-ordinates of grid points
+//   fprintf(fp, "DATASET STRUCTURED_GRID\n");
+//   fprintf(fp, "DIMENSIONS %6d %6d %6d\n", Nx, Ny, Nz );
+//   fprintf(fp, "POINTS %6d  float\n", num_points );
+//   for (l=0; l<=N; l++){
+//     float x, y, z;
+//     x = l*dx;
+//     y = l*dy;
+//     z = l*dz;
+//     fprintf(fp, "%f %f %f\n", x, y, z);
+//   }
+//
+//   // grid point values
+//   fprintf(fp,"POINT_DATA %6d\n", num_points);
+//   fprintf(fp,"SCALARS CONC  float  1\n");
+//   fprintf(fp,"LOOKUP_TABLE default\n");
+//   for (l=0; l <= N; l++) {
+//     fprintf(fp, "%f\n", conc[l]);
+//   }
+//
+//   fclose(fp);
+// }
 
 int main() {
 
