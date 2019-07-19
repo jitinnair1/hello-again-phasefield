64976e8dded9fe99c7f0b83d13335ff05713298d
Jitin <jitin.s.nair@gmail.com>
2019-07-20 02:08:29 +0530

    test to check correct PBC implementation

--- spinodal/laplacian.c
+++ spinodal/laplacian.c
@@ -9,9 +9,10 @@ float** laplacian(
     // use standard 5 point stencil
     int jp, jm, ip, im;
     int i, j;
+    int count1=0, count2=0, count3=0, count4=0;
 
-    for (i=0; i<nx; i++ ){
-      for (j=0; j<ny; j++ ){
+    for (i=0; i<=nx; i++ ){
+      for (j=0; j<=ny; j++ ){
         jp=j+1;
         jm=j-1;
 
@@ -20,22 +21,34 @@ float** laplacian(
 
         if(im < 0){
           im=nx;
+          count1+=1;
         }
 
         if(ip > nx){
-          ip=1;
+          ip=0;
+          count2+=1;
         }
 
         if(jm < 0){
           jm=ny;
+          count3+=1;
         }
 
         if(jp > ny){
-          jp=1;
+          jp=0;
+          count4+=1;
         }
 
         lap_con[i][j] =(conc[im][j] + conc[ip][j] + conc[i][jm] + conc[i][jp] -4.0*conc[i][j])/(dx*dy);
       }
     }
+
+    // // Test of Periodic Boundary Implementation
+    // printf("%d\n", count1 );
+    // printf("%d\n", count2 );
+    // printf("%d\n", count3 );
+    // printf("%d\n", count4 );
+    // printf("====\n");
     return lap_con;
+
   }
