2cae1f65d116f53a071013d74adeb9eba1ebe5fb
Jitin <jitin.s.nair@gmail.com>
2019-07-20 02:09:04 +0530

    added one more free energy function option

--- spinodal/free_energy.c
+++ spinodal/free_energy.c
@@ -5,8 +5,9 @@ float** free_energy(int nx, int ny, float **conc, float **dfdcon){
   int i, j;
   for (i=0; i<nx; i++ ){
     for(j=0; j<ny; j++){
-      dfdcon[i][j] = A*(2.0*conc[i][j]*(1-conc[i][j])*(1-conc[i][j])
-      -2.0*(conc[i][j]*conc[i][j])*(1.0-conc[i][j]));
+      // dfdcon[i][j] = A*(2.0*conc[i][j]*(1-conc[i][j])*(1-conc[i][j])
+      // -2.0*(conc[i][j]*conc[i][j])*(1.0-conc[i][j]));
+      dfdcon[i][j] = A*(conc[i][j] * conc[i][j])*(1-conc[i][j])*(1-conc[i][j]);
     }
   }
   return dfdcon;
