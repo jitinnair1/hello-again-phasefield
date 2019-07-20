24dd7b29e566fa5ae1983b41634876eff3d4ab94
Jitin <jitin.s.nair@gmail.com>
2019-07-21 01:57:29 +0530

    modified free energy for i, j sweep across grid

--- spinodal/free_energy.c
+++ spinodal/free_energy.c
@@ -1,16 +1,11 @@
 #include "spinodal.h"
 
-float** free_energy(int nx, int ny, float **conc, float **dfdcon){
+float** free_energy(int nx, int ny, float **conc, int i, int j){
   float A = 1.0;
-  int i, j;
-  for (i=0; i<=nx; i++ ){
-    for(j=0; j<=ny; j++){
-      // dfdcon[i][j] = A*(2.0*conc[i][j]*(1-conc[i][j])*(1-conc[i][j])
-      // -2.0*(conc[i][j]*conc[i][j])*(1.0-conc[i][j]));
-      // dfdcon[i][j] = A*(conc[i][j] * conc[i][j])*(1-conc[i][j])*(1-conc[i][j]);
-      dfdcon[i][j] = 2.0*A*conc[i][j]*(1.0 - conc[i][j])*(1-2.0*conc[i][j]);
-    }
-  }
+  // dfdcon[i][j] = A*(2.0*conc[i][j]*(1-conc[i][j])*(1-conc[i][j])
+  // -2.0*(conc[i][j]*conc[i][j])*(1.0-conc[i][j]));
+  // dfdcon[i][j] = A*(conc[i][j] * conc[i][j])*(1-conc[i][j])*(1-conc[i][j]);
+  dfdcon[i][j] = 2.0*A*conc[i][j]*(1.0 - conc[i][j])*(1-2.0*conc[i][j]);
   return dfdcon;
 
 }
