6e0c340973a0c8679d9256e7973acec4f00bb9ed
Jitin <jitin.s.nair@gmail.com>
2019-07-21 01:56:35 +0530

    modified laplacian for i, j sweep across grid

--- spinodal/laplacian.c
+++ spinodal/laplacian.c
@@ -4,44 +4,40 @@
 float** laplacian(
   int nx, int ny, int nz,
   float dx, float dy, float dz,
-  float **conc, float **lap_con){
+  float **conc, int i, int j){
 
     // use standard 5 point stencil
     int jp, jm, ip, im;
-    int i, j;
     int count1=0, count2=0, count3=0, count4=0;
 
-    for (i=0; i<=nx; i++ ){
-      for (j=0; j<=ny; j++ ){
-        jp=j-1;
-        jm=j+1;
+    jp=j-1;
+    jm=j+1;
 
-        ip=i-1;
-        im=i+1;
+    ip=i-1;
+    im=i+1;
 
-        if(ip < 0){
-          ip=nx;
-          count1+=1;
-        }
-
-        if(im > nx){
-          im=0;
-          count2+=1;
-        }
-
-        if(jp < 0){
-          jp=ny;
-          count3+=1;
-        }
-
-        if(jm > ny){
-          jm=0;
-          count4+=1;
-        }
-
-        lap_con[i][j] =(conc[im][j] + conc[ip][j] + conc[i][jm] + conc[i][jp] -4.0*conc[i][j])/(dx*dy);
-      }
+    if(ip < 0){
+      ip=nx;
+      count1+=1;
     }
+
+    if(im > nx){
+      im=0;
+      count2+=1;
+    }
+
+    if(jp < 0){
+      jp=ny;
+      count3+=1;
+    }
+
+    if(jm > ny){
+      jm=0;
+      count4+=1;
+    }
+
+    lap_con[i][j] =(conc[im][j] + conc[ip][j] + conc[i][jm] + conc[i][jp] -4.0*conc[i][j])/(dx*dy);
+
 
     // // Test of Periodic Boundary Implementation
     // printf("%d\n", count1 );
